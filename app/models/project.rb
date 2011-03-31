2787bdea73aead3986baa85effdf57175e06c253
cmthakur <cmthakur.r2s@gmail.com>
2011-03-31 08:52:29 +0545

    First commit of chandra

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :task_groups, :dependent => :destroy
 
-  # validates :name , :presence => true
+  validates :name , :presence => true
   validates :deadline , :presence => true
   validates :client , :presence => true
   validates :description, :length => { :minimum => 10 }
